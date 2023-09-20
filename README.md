8da167524112291bb99737024f4d39d85ebed4cb
James Gallagher <jamesg@jamesg.blog>
2023-09-20 11:50:36 +0100

    use fetch() instead of rf.js for requests, ui updates

---
+++ README.md
@@ -0,0 +1,21 @@
+![An example of the Roboflow Universe widget on a web page](example.png)
+
+# Roboflow Universe Widget
+
+The Roboflow Universe widget lets you embed links to your Roboflow models in your own website.
+
+## How to Use
+
+The Roboflow Universe widget is an `iframe` you can add to web pages.
+
+To use the widget add the following HTML snippet to your website:
+
+```html
+<iframe src="https://embed.roboflow.com/{project_id}/{model_version}" width="100%" height="100%"></iframe>
+```
+
+Apply styles where necessary so the widget fits on your web page.
+
+## License
+
+This project is licensed under an [MIT license](LICENSE).
--- assets/script.js
+++ assets/script.js
@@ -9,37 +9,30 @@ async function loadWidget() {
 
   console.log(publishable_key,modelId,version)
 
-  var model = await roboflow.auth({
-    publishable_key
-  }).load({
-    model: modelId,
-    version
-  })
+  // make a request to https://api.roboflow.com/tfjs/people-detection-o4rdr/7?publishable_key=rf_b0LW0UD0XvYuZKndeVpUrGoct322&host=stellasphere.github.io&u=22a2s-2btyisboaq1&v=0.2.26
+  fetch(`https://api.roboflow.com/tfjs/${modelId}/${version}?publishable_key=${publishable_key}&host=${location.host}&u=${navigator.userAgent}&v=0.2.26`)
+  .then(response => response.json())
+  .then(data => {
+    var metadata = data.tfjs;
+    title.innerText = metadata.name
+    image.style.backgroundImage = `
+  url("https://source.roboflow.com/${metadata.icon}/annotation-people.png"), url("https://source.roboflow.com/${metadata.icon}/thumb.jpg")
+    `
+    document.querySelector(`.tag-${metadata.type}`).style.display = "flex"
+    annotation.innerText = metadata.annotation
+    classesCount.innerText = `${metadata.classes.length} ${metadata.classes.length>1?"classes":"class"}`
 
-  var metadata = model.getMetadata()
-  console.log(metadata)
+    link = `https://universe.roboflow.com/leo-ueno/people-detection-o4rdr`
 
-  title.innerText = metadata.name
-  image.style.backgroundImage = `
-url("https://source.roboflow.com/${metadata.icon}/annotation-people.png"), url("https://source.roboflow.com/${metadata.icon}/thumb.jpg")
-  `
-  document.querySelector(`.tag-${metadata.type}`).style.display = "flex"
-  annotation.innerText = metadata.annotation
-  classesCount.innerText = `${metadata.classes.length} ${metadata.classes.length>1?"classes":"class"}`
-
-  link = `https://universe.roboflow.com/leo-ueno/people-detection-o4rdr`
-
-  tryLink.href = link
-  image.href = link
-  
-  copyLink.addEventListener("click",function() {
-    navigator.clipboard.writeText(link)
-  })
+    tryLink.href = link
+    image.href = link
+    
 
 
-  document.querySelectorAll("[class*=loading]").forEach(e=>{
-    e.classList.forEach((value,key,parent)=>{
-      if(value.includes("loading")) e.classList.remove(value)
+    document.querySelectorAll("[class*=loading]").forEach(e=>{
+      e.classList.forEach((value,key,parent)=>{
+        if(value.includes("loading")) e.classList.remove(value)
+      })
     })
   })
 }
